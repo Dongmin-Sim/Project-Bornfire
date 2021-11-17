797f9387dde209265bc9e89ed37342e64bb34bb6
이 동현B <smdf2545@naver.com>
2021-11-17 16:53:21 +0900

    ADD mypage feat ADD mypage - delete previous template
    -

--- main/my_page.py
+++ main/my_page.py
@@ -4,16 +4,4 @@ mypage = Blueprint("mypage", __name__)
 
 @mypage.route("/my-info")
 def get_myPage():
-    return render_template('my-page.html')
-
-@mypage.route("/my-emotion")
-def get_emotion():
-    return render_template('my-emotion.html', date = '8월')
-
-@mypage.route("/my-account")
-def get_acount():
-    return render_template('my-account.html')
-
-@mypage.route("/my-feed")
-def get_feed():
     return render_template('my-emotion.html')
