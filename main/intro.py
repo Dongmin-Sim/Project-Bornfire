8f07a3c6ef7c04313c1617c386cedc53b7711ccd
codongmin <codongmin@gmail.com>
2021-11-19 03:48:40 +0900

    Feat: intro page 화면 재구성
    1. 전체적인 구조 변경
      - 메인 타이틀 변경 (기존 모닥불 사진 -> 텍스트)
      - 본문구역 지정 후  바탕색 하얀색 수정
      - 서비스 소개에 대한 내용 추가 + 데이터 분석 밒 시각화 부분 컴팩트하게 수정
    2. chartjs 라인그래프, 바그래프, 도넛 그래프 업데이트 완료
      - 버튼 클릭시 바로 결과보여줄 수 있도록 변경(기존:업데이트 버튼을 눌러야 그래프 변경사항 반영됨)
      - 그래프 컬러 변경
    3. tableau 그래프 임베딩
      - tableau 그래프 임베딩 완료
    
    <추가 필요사항>
    1. 메인 타이틀 및 서비스 소개 부분 이미지 변경 필요
    2. tableau 그래프 비율 간격 수정 필요
    3. chartjs 그래프 제목 추가 필요
    4. "지역별 코로나블루" 내용 수정 필요
    
    -

--- main/intro.py
+++ main/intro.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, jsonify, request, redirect
-from .load_data import load_data
+from .load_data import load_patient_data, load_sexAge_data
 
 intro = Blueprint("intro", __name__)
 
@@ -7,14 +7,17 @@ intro = Blueprint("intro", __name__)
 def get_intro():
     return render_template('intro.html')
 
-@intro.route("/graph", methods=['GET'])
-def graph():
-    get_data = dict(request.args)
-    print(get_data)
-    category = get_data['option']
-
-    print(category)
+@intro.route("/patient", methods=['GET'])
+def patient():
+    btnValue = request.args.get('btnValue')
+    label, data = load_patient_data(btnValue)
     
-    label, data = load_data(category)
+    return jsonify(result=[label, data])
+
+
+@intro.route("/sexAge", methods=['GET'])
+def sexAge():
+    btnValue = request.args.get('btnValue')
+    label, data = load_sexAge_data(btnValue)
     
     return jsonify(result=[label, data])
--- main/load_data.py
+++ main/load_data.py
@@ -1,10 +1,35 @@
+from flask.globals import _lookup_app_object
 import pandas as pd
 
-def load_data(categroy):
-    csv_dir = f'static/assets/data/result_{categroy}_patient.csv'
+def load_patient_data(btnValue):
+    csv_dir = f'static/assets/data/patient/result_{btnValue}_patient.csv'
     
 
     df = pd.read_csv(csv_dir)
     label, data = df['date'].to_list(), df['total'].to_list()
     
+    return label, data
+
+
+def load_sexAge_data(btnValue):
+    male_csv_dir = f'static/assets/data/sexAge/counts_male.csv'
+    female_csv_dir = f'static/assets/data/sexAge/counts_female.csv'
+    pie_csv_div = f'static/assets/data/sexAge/total_cost.csv'
+
+    # bar chart
+    male_df = pd.read_csv(male_csv_dir)
+    female_df = pd.read_csv(female_csv_dir)
+
+    label = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80']
+    
+    male_data, female_data = male_df[btnValue].to_list(), female_df[btnValue].to_list()
+
+    # pie chart
+    cost_df = pd.read_csv(pie_csv_div)
+    cost_result = cost_df[btnValue].to_list()
+    
+    print(type(female_data), type(cost_result))
+
+    data = [male_data[1:], female_data[1:], cost_result]
+
     return label, data
