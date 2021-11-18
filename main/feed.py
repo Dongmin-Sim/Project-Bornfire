e783ebfc28cbe51c287630080b9647eb86aad41e
이 동현B <smdf2545@naver.com>
2021-11-18 17:53:24 +0900

    test

--- main/feed.py
+++ main/feed.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, render_template, jsonify, request, redirect
+import pymongo
+import datetime
 
 feed = Blueprint("feed", __name__)
+
+connection = pymongo.MongoClient('mongodb://localhost:27017/')
+db = connection.get_database("Bornfire")
+
+Feed_collection = db.get_collection("Feed_collection")
 
 @feed.route("/feed")
 def get_feed():
@@ -9,5 +16,32 @@ def get_feed():
 @feed.route('/feed', methods=['POST'])
 def post_feed():
     data = request.json
-    print(data)
-    return (jsonify("ok"))
+    nickname = str(data.get('nickname'))
+    context = str(data.get('context'))
+# 분석 툴이 들어 와서 emotion에 저장. 
+# '''
+# user_collection에 저장한다. 
+# ''' 
+    emotion = "good"
+    data = {
+        'Subject_number' : 1,
+        'nickname' : nickname,
+        'feed' : context,
+        "meta": {'thumps-up': 1, "createAt": datetime.datetime.utcnow()},
+        'emotion' : emotion
+    }
+
+    Feed_collection.insert_one(data)
+
+    cols = Feed_collection.find().limit(10)
+    
+    col_list = []
+    for col in cols:
+        col_list.append({
+            'nickname': col['nickname'],
+            'context' : col['feed'],
+            'thumbs-up': col['meta']['thumps-up']
+        })
+
+
+    return (jsonify(col_list))
--- static/js/feed.js
+++ static/js/feed.js
@@ -8,6 +8,7 @@ $('#button-addon2').click(function(){
         "nickname" : nickname,
         "context" : context
     };
+  
 
     $.ajax({
         type: 'POST',
@@ -15,8 +16,51 @@ $('#button-addon2').click(function(){
         data: JSON.stringify(postdata),
         dataType : 'json',
         contentType: "application/json",
-        success: function(data){
-            alert("통신 성공!!")
+        success: function(datas){
+            // alert("hi");
+            alert(datas[0]['nickname']);
+            console.log(datas[0]);
+            for(var i=0; i<length(datas); i++){
+                alert(datas[i]['nickname'])
+                var card = document.createElement("div");
+                card.setAttribute("id", "card");
+                card.setAttribute("class", "card feed-comment rounded-pill");
+                $('#post').append(card);
+
+                var card_header = document.createElement("div");
+                card_header.setAttribute("id", "card-header");
+                card_header.setAttribute("class", "card-header");
+                card_header.innerHTML = datas[i]['nickname'];
+                card.append(card_header);
+
+                var card_body = document.createElement("div");
+                card_body.setAttribute("id", "card-body");
+                card_body.setAttribute("class", "card-body");
+                card_header.append(card_body);
+
+
+                var blockquote = document.createElement("blockquote");
+                blockquote.setAttribute("class", "blockquote mb-0");
+                card_body.append(blockquote);
+
+                var p =  document.createElement("p");
+                p.innerHTML = datas[i]['context'];
+                blockquote.append(p);
+
+                var feed_button =  document.createElement("div")
+                feed_button.setAttribute("class", "feed-button");
+                blockquote.append(feed_button);
+
+                var button = document.createElement("button");
+                button.setAttribute("type", "button");
+                button.setAttribute("class", "btn btn-dark");
+                button.innerHTML = '🙌';
+                feed_button.append(button);
+
+                var span = document.createElement("span");
+                button.innerHTML = '&nbsp' + datas[i]['thumbs-up'];
+                button.append(span);
+            }
         },
         error: function(request, status, error){
             alert('ajax 통신 실패')
@@ -24,3 +68,4 @@ $('#button-addon2').click(function(){
         }
     })
 })
+
