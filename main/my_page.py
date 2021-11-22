a644a91622950f88d3a55ce027284af229d662b1
krc <krc@krcui-iMac.local>
2021-11-22 12:57:27 +0900

    mypage 작업

--- main/my_page.py
+++ main/my_page.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template, jsonify, request, redirect
+import pymongo
 
 mypage = Blueprint("mypage", __name__)
+
+connection = pymongo.MongoClient("mongodb://localhost:27017:/")
+db = connection.get_database("Bornfire")
+
+User_collection = db.get_collection("User_collection")
 
 @mypage.route("/my-page")
 def get_myPage():
     return render_template('my-page.html')
+
+
