32ddcfc48c0085b0e6d98488bc0a6c42ac981c41
krc <krc@krcui-iMac.local>
2021-11-22 15:32:03 +0900

    mongodb 코드 삭제

--- main/my_page.py
+++ main/my_page.py
@@ -3,11 +3,6 @@ import pymongo
 
 mypage = Blueprint("mypage", __name__)
 
-connection = pymongo.MongoClient("mongodb://localhost:27017:/")
-db = connection.get_database("Bornfire")
-
-User_collection = db.get_collection("User_collection")
-
 @mypage.route("/my-page")
 def get_myPage():
     return render_template('my-page.html')
