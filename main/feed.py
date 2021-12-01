fd91275e2e8586b5251e275343c789a8846126b2
이 동현B <smdf2545@naver.com>
2021-12-01 18:29:39 +0900

    refactoring

--- main/feed.py
+++ main/feed.py
@@ -5,7 +5,6 @@ from pymongo import cursor
 from .mongo_connect import db
 from .nickname import make_nickname
 import functools
-import time
 import random
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -29,15 +28,6 @@ sched.add_job(s_supply,'interval',seconds=60)
 sched.start()
 
 feed = Blueprint("feed", __name__)
-
-
-# def login_required(func):
-#     def wrapper():
-#         user = session.get('user_email')
-#         if user is None:
-#             return abort(404)
-#         func()
-#     return wrapper
 
 def login_required(func):
     @functools.wraps(func)
